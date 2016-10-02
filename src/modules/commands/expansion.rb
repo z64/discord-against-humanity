5b9a97e89cfeaf0c205b823f9a41b60ced887aa0
z64 <zachnowicki@gmail.com>
2016-10-02 00:50:14 -0400

    add sync

---
+++ src/modules/commands/expansion.rb
@@ -0,0 +1,53 @@
+module Bot
+  module DiscordCommands
+    # Inspect an expansion
+    module Expansion
+      extend Discordrb::Commands::CommandContainer
+      command(:expansion,
+              description: 'shows details about an expansion, and gives a sample',
+              usage: "#{BOT.prefix}expansion name",
+              min_args: 1) do |event, *name|
+        name = name.join(' ')
+        expansion = Database::Expansion.find(Sequel.ilike(:name, name))
+        unless expansion.nil?
+          # fetch a sample
+          sample = ''
+          if (expansion.answers.count.nonzero? && expansion.questions.count.nonzero?)
+            sample = expansion.questions.sample.text
+            if sample.scan(/\_/).count.nonzero?
+              sample.gsub!(/\_/) { "[#{expansion.answers.sample.text}]" }
+            else
+              sample << " [#{expansion.answers.sample.text}]"
+            end
+          elsif expansion.questions.count.nonzero?
+            sample = expansion.questions.sample.text
+          elsif expansion.answers.count.nonzero?
+            sample = expansion.answers.sample.text
+          end
+
+          # send formatted message to discord
+          event << "🗃 __Expansion__: **#{expansion.name}**"
+          event << '```cs'
+          event << "authors: #{expansion.authors}"
+          event << "cards: #{expansion.cards} "\
+                   "(#{expansion.questions.count} questions,"\
+                   " #{expansion.answers.count} answers)"
+          event << "sample: \"#{sample}\""
+          event << '```'
+          return
+        end
+        'expansion not found'
+      end
+
+      command(:expansions,
+              description: 'displays a list of expansions',
+              usage: "#{BOT.prefix}expansions") do |event|
+        expansions = Database::Expansion.all
+                                        .collect { |e| "`#{e.name} (#{e.cards})`" }
+                                        .join(', ')
+        event << '**Expansions**'
+        event << "#{expansions}"
+      end
+    end
+  end
+end
