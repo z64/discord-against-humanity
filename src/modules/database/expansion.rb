1a6bc45b5bb3b2c817ffa4144500d5b2738e7fd7
z64 <zachnowicki@gmail.com>
2016-10-02 00:50:45 -0400

    add #cards method to show total number of cards in a exp

--- src/modules/database/expansion.rb
+++ src/modules/database/expansion.rb
@@ -9,6 +9,14 @@ module Bot
       def after_create
         Discordrb::LOGGER.info("created expansion #{inspect}")
       end
+
+      # returns the total number of cards
+      # of the expansion
+      #
+      # @return [Integer] the number of cards
+      def cards
+        questions.count + answers.count
+      end
     end
   end
 end
