1001532cc95ae8ecf21a831083c1c17b551e01b1
z64 <zachnowicki@gmail.com>
2016-10-09 21:08:52 -0400

    only allow users to create one game

--- src/modules/commands/game.rb
+++ src/modules/commands/game.rb
@@ -5,41 +5,47 @@ module Bot
       extend Discordrb::Commands::CommandContainer
       # Creates a new game
       command(:new) do |event|
-        # Create channels
-        game_id = Database::Game.count + 1
-        channels = {
-          text: event.server.create_channel("game_#{game_id}", 0),
-          voice: event.server.create_channel("game_#{game_id}", 2)
-        }
+        unless Database::Player.where(discord_id: event.user.id)
+                               .any?(&:game_owner?)
+          # Create channels
+          game_id = Database::Game.count + 1
+          channels = {
+            text: event.server.create_channel("game_#{game_id}", 0),
+            voice: event.server.create_channel("game_#{game_id}", 2)
+          }
 
-        permissions = Discordrb::Permissions.new
-        permissions.can_read_messages = true
-        permissions.can_connect       = true
+          permissions = Discordrb::Permissions.new
+          permissions.can_read_messages = true
+          permissions.can_connect       = true
 
-        channels.each do |_, c|
-          c.define_overwrite(event.user, permissions, nil)
-          c.define_overwrite(event.server.roles.first, nil, permissions)
+          channels.each do |_, c|
+            c.define_overwrite(event.user, permissions, nil)
+            c.define_overwrite(event.server.roles.first, nil, permissions)
+          end
+
+          # Create game
+          game = Database::Game.create(
+            text_channel_id:  channels[:text].id,
+            voice_channel_id: channels[:voice].id,
+            max_points: CONFIG.max_points
+          )
+
+          # Create player
+          owner = Database::Player.create(
+            discord_id: event.user.id,
+            discord_name: event.user.distinct,
+            game: game
+          )
+
+          # Assign game owner
+          game.owner = owner
+          game.save
+
+          "**Created game: #{channels[:text].mention}**"
+        else
+          'You can only own one game at a time. '\
+          'Use `dah.end` to end an active game that you own.'
         end
-
-        # Create game
-        game = Database::Game.create(
-          text_channel_id:  channels[:text].id,
-          voice_channel_id: channels[:voice].id,
-          max_points: CONFIG.max_points
-        )
-
-        # Create player
-        owner = Database::Player.create(
-          discord_id: event.user.id,
-          discord_name: event.user.distinct,
-          game: game
-        )
-
-        # Assign game owner
-        game.owner = owner
-        game.save
-
-        "**Created game: #{channels[:text].mention}**"
       end
 
       # Invites users to game
