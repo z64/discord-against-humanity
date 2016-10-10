948d2e421d8d85a3d78b07782202e59a69fb5cf9
z64 <zachnowicki@gmail.com>
2016-10-10 03:04:34 -0400

    add Game#end! and delete_channels abstraction

--- src/modules/database/game.rb
+++ src/modules/database/game.rb
@@ -10,8 +10,7 @@ module Bot
 
       # Clean up before destruction
       def before_destroy
-        text_channel.delete
-        voice_channel.delete
+        delete_channels
       end
 
       # Fetch channel from bot cache
@@ -22,6 +21,24 @@ module Bot
       # Fetch channel from bot cache
       def voice_channel
         BOT.channel(voice_channel_id)
+      end
+
+      # Deletes Discord channels for the game
+      def delete_channels
+        text_channel.delete
+        voice_channel.delete
+      end
+
+      # End a game. Destroys the game
+      # if it has no decided winner, otherwise
+      # keep the Game for history and just clean
+      # up the channels.
+      def end!
+        if winner.nil?
+          destroy
+        else
+          delete_channels
+        end
       end
     end
   end
