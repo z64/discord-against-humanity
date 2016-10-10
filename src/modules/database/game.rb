15fd4ad9f49253af596e02213654060886814c4c
z64 <zachnowicki@gmail.com>
2016-10-09 20:21:15 -0400

    specify player class in game associations

--- src/modules/database/game.rb
+++ src/modules/database/game.rb
@@ -2,9 +2,9 @@ module Bot
   module Database
     # A Game
     class Game < Sequel::Model
-      many_to_one :owner
-      many_to_one :czar
-      many_to_one :winner
+      many_to_one :owner,  class: "::Bot::Database::Player"
+      many_to_one :czar,   class: "::Bot::Database::Player"
+      many_to_one :winner, class: "::Bot::Database::Player"
       one_to_many :players
       one_to_many :rounds
     end
