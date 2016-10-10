0aee0334ca3acb7277481b2741255da9c4c26f92
z64 <zachnowicki@gmail.com>
2016-10-09 20:30:15 -0400

    add initial game creation command

--- src/modules/commands/game.rb
+++ src/modules/commands/game.rb
@@ -5,6 +5,40 @@ module Bot
       extend Discordrb::Commands::CommandContainer
       # Creates a new game
       command(:new) do |event|
+        # Create channels
+        game_id = Database::Game.count + 1
+        channels = {
+          text: event.server.create_channel("game_#{game_id}", 0),
+          voice: event.server.create_channel("game_#{game_id}", 2)
+        }
+
+        permissions = Discordrb::Permissions.new
+        permissions.can_read_messages = true
+        permissions.can_connect       = true
+
+        channels.each do |_, c|
+          c.define_overwrite(event.user, permissions, nil)
+          c.define_overwrite(event.server.roles.first, nil, permissions)
+        end
+
+        # Create game
+        game = Database::Game.create(
+          text_channel_id:  channels[:text].id,
+          voice_channel_id: channels[:voice].id
+        )
+
+        # Create player
+        owner = Database::Player.create(
+          discord_id: event.user.id,
+          discord_name: event.user.distinct,
+          game: game
+        )
+
+        # Assign game owner
+        game.owner = owner
+        game.save
+
+        "**Created game: #{channels[:text].mention}**"
       end
 
       # Invites users to game
