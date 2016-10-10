682135c0aa9802a47d818ca02cea5b286165621c
z64 <zachnowicki@gmail.com>
2016-10-09 20:29:58 -0400

    add cache and channel cleanup methods

--- src/modules/database/game.rb
+++ src/modules/database/game.rb
@@ -7,6 +7,22 @@ module Bot
       many_to_one :winner, class: '::Bot::Database::Player'
       one_to_many :players
       one_to_many :rounds
+
+      # Clean up before destruction
+      def before_destroy
+        text_channel.delete
+        voice_channel.delete
+      end
+
+      # Fetch channel from bot cache
+      def text_channel
+        BOT.channel(text_channel_id)
+      end
+
+      # Fetch channel from bot cache
+      def voice_channel
+        BOT.channel(voice_channel)
+      end
     end
   end
 end
