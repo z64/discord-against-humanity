650b778acd3cddd30c1b9b946ad584ffb0d5ca36
z64 <zachnowicki@gmail.com>
2016-10-11 20:40:33 -0400

    add missing question assoc

--- src/modules/database/round.rb
+++ src/modules/database/round.rb
@@ -5,6 +5,7 @@ module Bot
       many_to_one :game
       one_to_many :plays
       many_to_one :play
+      many_to_one :question
 
       # Pick a random question on creation. (temporary)
       def before_create
