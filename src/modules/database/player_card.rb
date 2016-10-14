1349ff9ea5b155f5788528db3809aec47c641fc8
z64 <zachnowicki@gmail.com>
2016-10-14 00:03:38 -0400

    add unplayed? and played?

--- src/modules/database/player_card.rb
+++ src/modules/database/player_card.rb
@@ -4,6 +4,16 @@ module Bot
     class PlayerCard < Sequel::Model
       many_to_one :player
       many_to_one :answer
+
+      # If the card hasn't been played
+      def unplayed?
+        !played
+      end
+
+      # If the card has been played
+      def played?
+        played
+      end
     end
   end
 end
