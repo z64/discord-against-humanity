0a1c67a9b0bad3ee05ec454c7d69f1e4c3b6aa45
z64 <zachnowicki@gmail.com>
2016-10-14 01:57:16 -0400

    remove temporary before_create method

--- src/modules/database/round.rb
+++ src/modules/database/round.rb
@@ -6,12 +6,6 @@ module Bot
       one_to_many :plays
       many_to_one :play
       many_to_one :question
-
-      # Pick a random question on creation. (temporary)
-      def before_create
-        super
-        self.question = Question.all.sample
-      end
     end
   end
 end
