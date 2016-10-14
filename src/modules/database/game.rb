8b1fb469c319408d80e6f21ff47636725c88c734
z64 <zachnowicki@gmail.com>
2016-10-14 01:56:56 -0400

    clean up enumerator in next_round!

--- src/modules/database/game.rb
+++ src/modules/database/game.rb
@@ -45,10 +45,7 @@ module Bot
 
       # Creates a new round and distrubtes player cards
       def next_round!
-        players.each do |p|
-          p.restock_hand!
-        end
-
+        players.each(&:restock_hand!)
         add_round Round.create(question: available_questions.sample)
       end
 
