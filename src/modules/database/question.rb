b766b359354389507e8b4bfb11b01ac82d2bdf34
z64 <zachnowicki@gmail.com>
2016-10-17 16:12:34 -0400

    add #substitute to Question

--- src/modules/database/question.rb
+++ src/modules/database/question.rb
@@ -8,6 +8,12 @@ module Bot
       def after_create
         Discordrb::LOGGER.info("created question #{inspect}")
       end
+
+      # Substitutes a set of answers
+      # into a question's slots
+      def substitute(answers)
+        text.gsub(/_/) { "`#{answers.shift.text}`" }
+      end
     end
   end
 end
