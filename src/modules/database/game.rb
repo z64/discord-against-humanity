94aac634b7a933ba71105250b2c1e557cc832026
z64 <zachnowicki@gmail.com>
2016-10-17 18:29:41 -0400

    remove czar assoc from Game

--- src/modules/database/game.rb
+++ src/modules/database/game.rb
@@ -3,7 +3,6 @@ module Bot
     # A Game
     class Game < Sequel::Model
       many_to_one :owner,  class: '::Bot::Database::Player'
-      many_to_one :czar,   class: '::Bot::Database::Player'
       many_to_one :winner, class: '::Bot::Database::Player'
       one_to_many :players
       one_to_many :rounds
