f2d18a1cb00cde6089594741052d38186cb8e2ca
z64 <zachnowicki@gmail.com>
2016-10-17 19:59:47 -0400

    start implementing pick

--- src/modules/events/play.rb
+++ src/modules/events/play.rb
@@ -9,7 +9,29 @@ module Bot
       end
 
       # Picks a winning card
-      message(starts_with: /pick/i) do |event|
+      message(start_with: /pick/i) do |event|
+        number = event.message.content.split(' ').last.to_i - 1
+        player = Database::Player.find_active(event.user.id)
+        unless player.nil? || !player.game.started
+          if player.czar?
+            unless player.game.current_round.enough_responses?
+              event.respond('Still waiting for other players to pick cards.. :eyes:')
+            else
+              event.respond(':ballot_box_with_check:')
+            end
+          else
+            if event.channel.pm?
+              card = player.unplayed_cards.at(number)
+              unless card.nil? || player.enough_responses?
+                player.game.current_round.add_play(player_card: card)
+                player.game.current_round.update_message!
+                event.respond(':ballot_box_with_check:')
+              else
+                event.respond(':negative_squared_cross_mark:')
+              end
+            end
+          end
+        end
       end
     end
   end
