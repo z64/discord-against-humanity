0fe2e55b42591367230ac89c7b184a10c103b92f
z64 <zachnowicki@gmail.com>
2016-10-17 20:48:34 -0400

    correctly apply winner, correctly play card, and announce all cards have been played

--- src/modules/events/play.rb
+++ src/modules/events/play.rb
@@ -13,15 +13,27 @@ module Bot
             unless player.game.current_round.enough_responses?
               event.respond('Still waiting for other players to pick cards.. :eyes:')
             else
-              event.respond(':ballot_box_with_check:')
+              response = player.game.current_round.responses.at(number)
+              unless response.nil?
+                player.game.current_round.update(winner: response.first.player)
+                player.game.current_round.update_message!
+              else
+                event.respond(':negative_squared_cross_mark:')
+              end
             end
           else
             if event.channel.pm?
               card = player.unplayed_cards.at(number)
-              unless card.nil? || player.enough_responses?
-                player.game.current_round.add_play(player_card: card)
+              unless card.nil? || player.enough_responses? || player.game.current_round.enough_responses?
+                card.play!
                 player.game.current_round.update_message!
                 event.respond(':ballot_box_with_check:')
+                if player.game.current_round.enough_responses?
+                  player.game.text_channel.send_message(
+                    "#{player.game.current_round.czar.discord_user.mention}, all cards are in!\n"\
+                    "Pick a winning card with `pick [number]`!"
+                  )
+                end
               else
                 event.respond(':negative_squared_cross_mark:')
               end
