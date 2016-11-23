d92ece8ee960bb4f123691b7979ef9606e523e8a
z64 <zachnowicki@gmail.com>
2016-11-22 19:36:19 -0500

    try to predict game ID for channel name

--- src/modules/commands/game.rb
+++ src/modules/commands/game.rb
@@ -10,7 +10,7 @@ module Bot
           'Use `dah.end` to end an active game that you host.'
         else
           # Create channels
-          game_id = Database::Game.count + 1
+          game_id = Database::Game.last.id + 1
           channels = {
             text: event.server.create_channel("game_#{game_id}", 0),
             voice: event.server.create_channel("game_#{game_id}", 2)
