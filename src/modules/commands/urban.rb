a9bada1c47c2a22efc4f68ff9f7ff4ab351df9eb
Jos Spencer <crazyandroid101@gmail.com>
2016-11-29 11:54:19 +0800

    Moved most of the functionality into methods

--- src/modules/commands/urban.rb
+++ src/modules/commands/urban.rb
@@ -5,8 +5,12 @@ module Bot
       extend Discordrb::Commands::CommandContainer
 
       class UrbanDictionary::Definition
-        def long?
-          [text.length, example.length].any? { |e| e > 1024 }
+        def long?(text)
+          return true if text.length > 1024
+        end
+        def shorten(text,extra='')
+          maxlength = 1023 - extra.length
+          return text[0..maxlength].strip + extra
         end
       end
 
@@ -17,12 +21,14 @@ module Bot
         word   = UrbanDictionary.random.first if term.empty?
         word ||= UrbanDictionary.define(term).first
         next "Couldn't find anything for `#{term}` 😕" unless word
+        url = "... [(Read More)](#{word.url})"
+        word.example.gsub!('*','')
 
         event.channel.send_embed do |e|
           e.add_field name: 'Definition',
-            value: word.long? ? "#{word.text[0..600].strip}... [(Read More)](#{word.url})" : word.text, inline: false
+            value: word.long?(word.text) ? word.shorten(word.text,url) : word.text, inline: false
           e.add_field name: 'Example',
-            value: "*#{word.long? ? word.example.gsub('*','')[0..600].strip + '...' : word.example.gsub('*','')}*", inline: false if word.example
+            value: word.long?(word.example) ? word.shorten(word.example,'...') : word.example, inline: false if word.example
           e.add_field name: "\u200B", value: "⬆️ `#{word.thumbs_up}` ⬇️ `#{word.thumbs_down}`", inline: false
           e.author = {
             icon_url: 'http://www.dimensionalbranding.com/userfiles/urban_dictionary.jpg',
