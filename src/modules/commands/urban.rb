bb71be07cf95d54a5970f2c323256c1ef4c4c7fe
Jos Spencer <crazyandroid101@gmail.com>
2016-12-01 17:23:25 +0800

    Seperated the long? command into two seperate ones

--- src/modules/commands/urban.rb
+++ src/modules/commands/urban.rb
@@ -4,15 +4,15 @@ module Bot
     module Urban
       extend Discordrb::Commands::CommandContainer
 
-      class UrbanDictionary::Definition
-        def long?(text)
-          return true if text.length > 1024
+
+        class UrbanDictionary::Definition
+          def long_text?
+            text.length > 1023
+          end
+          def long_example?
+            example.length > 1023
+          end
         end
-        def shorten(text,extra='')
-          maxlength = 1023 - extra.length
-          return text[0..maxlength].strip + extra
-        end
-      end
 
       command([:ud, :urban],
               description: 'look up a word on Urban Dictionary',
@@ -22,21 +22,23 @@ module Bot
         word ||= UrbanDictionary.define(term).first
         next "Couldn't find anything for `#{term}` 😕" unless word
         url = "... [(Read More)](#{word.url})"
-        word.example.gsub!('*','')
+        word.example.delete!('*')
 
         event.channel.send_embed do |e|
           e.add_field name: 'Definition',
-            value: word.long?(word.text) ? word.shorten(word.text,url) : word.text, inline: false
-          e.add_field name: 'Example',
-            value: word.long?(word.example) ? word.shorten(word.example,'...') : word.example, inline: false if word.example
+                      value: word.long_text? ? truncate(word.text, url) : word.text, inline: false
+          if word.example
+            e.add_field name: 'Example',
+                        value: word.long_example? ? truncate(word.example, '...') : word.example, inline: false
+          end
           e.add_field name: "\u200B", value: "⬆️ `#{word.thumbs_up}` ⬇️ `#{word.thumbs_down}`", inline: false
           e.author = {
             icon_url: 'http://www.dimensionalbranding.com/userfiles/urban_dictionary.jpg',
             name: word.word,
             url: word.url
           }
-          e.footer = { text: "Author: #{word.author}"}
-          e.color = 5800090
+          e.footer = { text: "Author: #{word.author}" }
+          e.color = 5_800_090
         end
       end
     end
